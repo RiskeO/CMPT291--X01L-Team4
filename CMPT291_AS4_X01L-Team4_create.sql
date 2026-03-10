5ae8660bb782e7a4d93de5418ec1ae91506a99c4
RiskeO <riskeo@mymacewan.ca>
2026-03-10 16:07:58 -0600

    Movie create & insert (Test)

---
+++ CMPT291_AS4_X01L-Team4_create.sql
@@ -0,0 +1,14 @@
+﻿DROP TABLE [Movie]
+GO
+create Table Movie(
+	MovieID int IDENTITY(1,1) Primary Key,
+	MovieName varchar(255) not null,
+	Fee decimal(10,2) not null,
+	CopyNum int not null ,
+	CopyRented int not null default 0,
+	Rating decimal(3,2) not null,
+	Available as (CopyNum - CopyRented) Persisted,
+	Constraint  chk_Rating check (Rating >= 0 and Rating <= 10),
+	Constraint  chk_Copy check (CopyNum >= 0 and CopyRented >= 0 and CopyRented <= CopyNum),
+	Constraint  chk_Fee check (Fee >= 0)
+)
---
+++ CMPT291_AS4_X01L-Team4_insert.sql
@@ -0,0 +1,8 @@
+﻿delete Movie
+
+/* Movie */
+insert into Movie values ('The Shawshank Redemption', 2.99, 5, 2, 9.3)
+insert into Movie values ('The Godfather', 3.99, 3, 1, 9.2)
+insert into Movie values ('The Dark Knight', 2.49, 4, 3, 9.0)
+
+select * from Movie
