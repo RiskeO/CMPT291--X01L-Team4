2a79fb389785a76771a63bbdc4f6370aad8c12b8
stevanovicf-mac <stevanovicf@mymacewan.ca>
2026-03-12 11:24:13 -0600

    Added Orders table and made sequences for Orders, Actor and Movies

--- CMPT291_AS4_X01L-Team4_create.sql
+++ CMPT291_AS4_X01L-Team4_create.sql
@@ -4,9 +4,11 @@
 -- Clean up DB schema
 --
 
-DROP SEQUENCE IF EXISTS Employee_EmployeeID_Seq;
-DROP SEQUENCE IF EXISTS Customer_CustomerID_Seq;
-DROP SEQUENCE IF EXISTS Movie_MovieID_Seq;
+DROP SEQUENCE IF EXISTS Orders_Order_ID_Seq;
+DROP SEQUENCE IF EXISTS Actor_Actor_ID_Seq;
+DROP SEQUENCE IF EXISTS Employee_Employee_ID_Seq;
+DROP SEQUENCE IF EXISTS Customer_Customer_ID_Seq;
+DROP SEQUENCE IF EXISTS Movie_Movie_ID_Seq;
 
 DROP TABLE IF EXISTS Rate_Actor;
 DROP TABLE IF EXISTS Queue_Up;
@@ -34,7 +36,7 @@ PRIMARY KEY( Employee_ID ),
 UNIQUE ( SSN ),
 );
 
-CREATE SEQUENCE Employee_EmployeeID_Seq START WITH 1000 INCREMENT BY 1;
+CREATE SEQUENCE Employee_Employee_ID_Seq START WITH 1000 INCREMENT BY 1;
 
 CREATE TABLE Employee_Phone (
 Employee_ID INT NOT NULL,
@@ -62,7 +64,7 @@ Credit_Card_Number NCHAR(16),
 PRIMARY KEY(Customer_ID)
 );
 
-CREATE SEQUENCE Customer_CustomerID_Seq START WITH 1000 INCREMENT BY 1;
+CREATE SEQUENCE Customer_Customer_ID_Seq START WITH 1000 INCREMENT BY 1;
 
 CREATE TABLE Customer_Phone (
 Customer_ID INT NOT NULL,
@@ -73,7 +75,7 @@ FOREIGN KEY( Customer_ID ) REFERENCES Customer( Customer_ID )
 );
 
 CREATE TABLE Movie(
-	Movie_ID INT IDENTITY(1,1) PRIMARY KEY,
+	Movie_ID INT NOT NULL PRIMARY KEY,
 	Movie_Name VARCHAR(255) NOT NULL,
 	Fee DECIMAL(10,2) NOT NULL,
 	Copy_Num INT NOT NULL ,
@@ -85,6 +87,8 @@ CREATE TABLE Movie(
 	CONSTRAINT  chk_Fee CHECK (Fee >= 0)
 );
 
+CREATE SEQUENCE Movie_Movie_ID_Seq START WITH 1000 INCREMENT BY 1;
+
 CREATE TABLE Actor (
 Actor_ID INT IDENTITY(1,1),
 Last_Name VARCHAR(40) NOT NULL,
@@ -94,6 +98,8 @@ Gender VARCHAR(10) NOT NULL
 Date_Of_Birth DATE NOT NULL,
 PRIMARY KEY( Actor_ID )
 );
+
+CREATE SEQUENCE Actor_Actor_ID_Seq START WITH 1000 INCREMENT BY 1;
 
 CREATE TABLE Appeared_In (
 Actor_ID INT NOT NULL,
@@ -108,7 +114,24 @@ Actor_ID INT NOT NULL,
 Order_ID INT NOT NULL,
 A_Rate INT
 	CHECK( (A_Rate >= 1 AND A_Rate <= 5) OR A_Rate = NULL),
-PRIMARY KEY ( Actor_ID, Order_ID )
+PRIMARY KEY ( Actor_ID, Order_ID ),
 FOREIGN KEY ( Actor_ID ) REFERENCES Actor( Actor_ID ),
 FOREIGN KEY ( Order_ID ) REFERENCES Order( Order_ID )
 );
+
+CREATE TABLE Orders (
+Order_ID INT NOT NULL,
+Customer_ID INT NOT NULL,
+Employee_ID INT NOT NULL,
+Movie_ID INT NOT NULL,
+Checkout_Date DATE NOT NULL DEFAULT(getdate()),
+Return_Date DATE NOT NULL,
+Movie_Rating INT
+	CHECK( (Movie_Rating >= 1 AND Movie_Rating <= 5) OR Movie_Rating = NULL),
+PRIMARY KEY ( Order_ID ),
+FOREIGN KEY ( Customer_ID ) REFERENCES Customer( Customer_ID ),
+FOREIGN KEY ( Employee_ID ) REFERENCES Employee( Employee_ID ),
+FOREIGN KEY ( Movie_ID ) REFERENCES Movie( Movie_ID )
+);
+
+CREATE SEQUENCE Orders_Order_ID_Seq START WITH 1000 INCREMENT BY 1;
